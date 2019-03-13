bb8290b84ba1d6c1382190dfe3a8e5f6155b6f26
Okall Vivian <vivieokall@gmail.com>
2019-03-13 17:46:25 +0300

    rectify url linking

--- gram/urls.py
+++ gram/urls.py
@@ -6,14 +6,15 @@ from django.conf import settings
 
 
 urlpatterns=[
-    url(r'^$',views.index,name='index'),
-    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^$', views.signup, name='signup'),
+    # url(r'signup', views.signup, name='signup'),
+    url(r'^index/',views.index, name='index'),
     url(r'^upload/',views.upload, name='upload'),
-    url(r'^update_profile/$',views.update_profile, name='update_profile'),
-    url(r'^accounts/profile/$',views.profile, name='profile'),
+    url(r'^update_profile/',views.update_profile, name='update_profile'),
+    url(r'^accounts/profile/',views.profile, name='profile'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
-    
+
 ]
 
 
